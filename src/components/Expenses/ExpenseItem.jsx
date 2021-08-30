b9dde6989aa0ff500c219c0fe55071dbe847df25
algoroloro <algoro9697@gmail.com>
2021-08-30 17:05:37 +0200

    changing function syntax

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,7 +3,7 @@ import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
-export default function ExpenseItem (props) {
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -14,3 +14,5 @@ export default function ExpenseItem (props) {
     </Card>
   )
 }
+
+export default ExpenseItem
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Card.css'
 
-export default function Card (props) {
+const Card = (props) => {
   return (
     <div className={`card ${props.className}`}>
       {props.children}
     </div>
   )
 }
+
+export default Card
